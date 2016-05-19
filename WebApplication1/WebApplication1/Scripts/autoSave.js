1fb112455a87949fb4a3f056893ea46a1e6d33e6
Alex Danielsson <danielsson.alex@hotmail.com>
2016-05-19 16:26:54 +0200

    worked with autosave

---
+++ WebApplication1/WebApplication1/Scripts/autoSave.js
@@ -0,0 +1,23 @@
+﻿var File1 = {
+    Name: "one",
+    Content: "<div id='asda'></div>",
+    fileEnding: ".html"
+}
+
+var File2 = {
+    Name: "two",
+    Content: "<div id='asda'></div>",
+    fileEnding: ".html"
+}
+
+var File3 = {
+    Name: "three",
+    Content: "<div id='asda'></div>",
+    fileEnding: ".html"
+}
+
+var File4 = {
+    Name: "four",
+    Content: "<div id='asda'></div>",
+    fileEnding: ".html"
+}
