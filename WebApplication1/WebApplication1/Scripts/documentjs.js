0fd51bd6378973ef5f44ee6960d6a2fc04c75081
Erik Petersson <erik__petersson@hotmail.com>
2016-05-20 10:56:30 +0200

    uppdaterade Task-53 med koden från förra git-projekt.

---
+++ WebApplication1/WebApplication1/Scripts/documentjs.js
@@ -0,0 +1,11 @@
+﻿// trigger extension
+   ace.require("ace/ext/language_tools");
+   var editor = ace.edit("editor");
+   editor.session.setMode("ace/mode/javascript");
+   editor.setTheme("ace/theme/tomorrow");
+   // enable autocompletion and snippets
+   editor.setOptions({
+       enableBasicAutocompletion: true,
+       enableSnippets: true,
+       enableLiveAutocompletion: true
+   });
