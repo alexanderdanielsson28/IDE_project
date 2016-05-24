caa5d144f0a47281930b249b5db15a2b5e706f74
Erik Petersson <erik__petersson@hotmail.com>
2016-05-24 09:43:10 +0200

    Fixat så att man kan koda i fullscreen mode och smidigt kunna byta från fullscreen till window mode.

--- WebApplication1/WebApplication1/Scripts/documentjs.js
+++ WebApplication1/WebApplication1/Scripts/documentjs.js
@@ -9,3 +9,31 @@
        enableSnippets: true,
        enableLiveAutocompletion: true
    });
+   function full() {
+       document.getElementById('editor').style.width = "100%";
+       document.getElementById('editor').style.height = "100%";
+       document.getElementById('editor').style.position = "absolute";
+       document.getElementById('fullwidth1pxheight').style.float = "none";
+       document.getElementById('fullwidth1pxheight').style.display = "none";
+       document.getElementById('tinyboxtwin').style.display = "block";
+       document.getElementById('tinyboxtwin').style.zIndex = "60";
+       document.getElementById('tinyboxtwin').style.position = "absolute";
+       document.getElementById('twinbox').style.display = "block";
+   }
+   function onf() {
+       document.getElementById('fullwidth1pxheight').style.display = "none";
+       document.getElementById('tinyboxtwin').style.display = "block";
+       document.getElementById('twinbox').style.display = "block";
+   }
+   function onft() {
+       document.getElementById('fullwidth1pxheight').style.display = "block";
+       document.getElementById('fullwidth1pxheight').style.zIndex = "60";
+       document.getElementById('fullwidth1pxheight').style.position = "relative";
+       document.getElementById('tinyboxtwin').style.display = "none";
+       document.getElementById('twinbox').style.display = "none";
+   }
+   function revert() {
+       document.getElementById('editor').style.width = "900px";
+       document.getElementById('editor').style.height = "600px";
+       document.getElementById('editor').style.position = "relative";
+   }
