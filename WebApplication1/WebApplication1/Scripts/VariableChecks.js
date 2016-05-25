72b2374e171f3f6392b41c7164a74b4d655b51f1
Johan Stening <johan.stening@hotmail.com>
2016-05-25 11:06:01 +0200

    Changing branch, no new features

--- WebApplication1/WebApplication1/Scripts/VariableChecks.js
+++ WebApplication1/WebApplication1/Scripts/VariableChecks.js
@@ -1,21 +1,21 @@
-﻿var JSVarAnyStartAndEnd = /^.*?var[\s]{1,}[a-zA-Z]{1,}.*?$/;
-var JSVarSpecific = /\bvar[\s]{1,}[a-zA-Z]{1,}\b/g
-var JSVarSpecific2 = /\bvar[\s]{1,}([a-zA-Z]){1,}.*\1\b/g
+﻿var JSDeclarations = /\b(var|function)[\s]{1,}[a-zA-Z]{1,}\b/g
 
-editor.session.setOption("useWorker", false)
-console.log(editor.session.getOption("useWorker"))
+
+$(document).ready(function () {
+    editor.session.setOption("useWorker", false)
+})
 
 window.setInterval(function () {
     var duplicatesFound = [];
     var uniqueNames = [];
     var text = editor.getValue();
-    var matches = text.match(JSVarSpecific);
-    //var matches = JSVarSpecific2.exec(text);
-    //console.log(matches)
+    var matches = text.match(JSDeclarations);
     if (matches) {
+        console.log(matches)
         for (var i = 0; i < matches.length; i++) {
-            matches[i] = matches[i].replace(/var\s+/g, '');
+            matches[i] = matches[i].replace(/\s+/g, '');
         }
+        console.log(matches)
         function goThroughArray(arr) {
             for (var i = 0; i < arr.length; i++) {
                 if ((arr.lastIndexOf(arr[i]) != i) &&
@@ -25,12 +25,8 @@ window.setInterval(function () {
             }
             return duplicatesFound;
         }
+        
         console.log(goThroughArray(matches))
-         
-       //var session =  ace.createEditSession(editor.getValue(), "ace/mode/javascript")
-        //console.log(session)
-        //var row = new Range(found.start.row + 1, found.start.column - 1, found.end.row + 1, found.end.column);
-        //var search = new Search();
         var annotationsArray = [];
          
         for (var i = 0; i < duplicatesFound.length; i++) {
@@ -43,89 +39,15 @@ window.setInterval(function () {
                     rowsNotice += ",";
                 }
             }
-
-           
-
-            console.log(found);
-            
             for (var j = 0; j < found.length; j++) {
                 annotationsArray.push({
                     row: found[j].start.row,
                     column: 0,
-                    text: "The variable '" + duplicatesFound[i] + "' has already been declared on lines:" + rowsNotice,
-                    type: "error"
+                    text: "'"+duplicatesFound[i] + "' has already been declared on lines:" + rowsNotice,
+                    type: "info"
                 });
             }
         }
         editor.getSession().setAnnotations(annotationsArray);
     }
-    
-
-            //range.start.column = 0
-            //range.end.column = Number.MAX_VALUE
-            //editor.session.replace(range, "x" + editor.session.getLine(range.start.row) + "x")
-            // editor.selection.setRange(range)
-
-            //console.log(range)
-            
-            /*for (var j = 0; j < range.length; j++) {
-                editor.session.setAnnotations([{
-                    row: range.start.row,
-                    column: range.start.column,
-                    text: "The variable '" + duplicatesFound[j] + "' is already declared.",
-                    type: "info"
-                }])
-            }
-            console.log(range)*/
-    
-    /*
-    $.each(matches, function (i, el) {
-        if ($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
-        if ($.inArray(el, uniqueNames) > 0) {
-            duplicatesFound.push(el);
-        }
-    });
-
-    console.log("duplicates: "+duplicatesFound)
-    console.log("uniques: " + uniqueNames)*/
-    /*var uniqueArray = matches.sort().filter(function (item, pos) {
-        if (!matches.indexOf(item) == pos) {
-            duplicatesFound.push(item);
-        }
-        //duplicatesFound.push(matches.indexOf(item) != pos);
-        return matches.indexOf(item) == pos;
-    })*/
-
-    /*
-var i,
-len = matches.length,
-out = [],
-obj = {};
-
-for (i = 0; i < len; i++) {
-    matches[i] = matches[i].replace(/ /g, '');
-    obj[matches[i]] = 0;
-    if (true) {
-    }
-}
-for (i in obj) {
-    out.push(i);
-}
-    console.log(obj)
-console.log(out);
-*/
-
-
 }, 1000);
-
-
-
-
-/*editor.find('needle', {
-    backwards: false,
-    wrap: true,
-    caseSensitive: true,
-    range: null,
-    wholeWord: true,
-    regExp: false
-})*/
