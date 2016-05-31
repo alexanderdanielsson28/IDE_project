1fb1921c6447c77b230688a425b7d9bfd25e46c7
Alex Danielsson <danielsson.alex@hotmail.com>
2016-05-31 11:14:16 +0200

    autoSave Done

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,29 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Web.Services;
+using WebApplication1.Models;
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
-       
-
-        public ActionResult About()
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Index(string reportName)
+        {
+            aboutModel data = new aboutModel();
+            data.html = reportName;
+            //check for reportName parameter value now
+            //to do : Return something
+            return RedirectToAction("About", data);
+          
+        }
+        [ValidateInput(false)]
+        public ActionResult About(aboutModel data)
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return View(data);
         }
-
+      
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+        //controller
+     
+
     }
 }
