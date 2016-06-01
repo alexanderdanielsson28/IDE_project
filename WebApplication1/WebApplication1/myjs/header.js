64fa5a7a66c21ad05ba6f614d8143ed5cd17ea63
Wigge <wigge_94@live.se>
2016-06-01 10:26:55 +0200

    Remade terminal, works with themes now

--- WebApplication1/WebApplication1/myjs/header.js
+++ WebApplication1/WebApplication1/myjs/header.js
@@ -1,29 +1,17 @@
 ﻿
-
-// trigger extension
-ace.require("ace/ext/language_tools");
-var editor = ace.edit("editor");
-editor.session.setMode("ace/mode/javascript");
-editor.setTheme("ace/theme/tomorrow");
-// enable autocompletion and snippets
-editor.setOptions({
-    enableBasicAutocompletion: true,
-    enableSnippets: true,
-    enableLiveAutocompletion: true
-});
 function black() {
     document.body.style.backgroundColor = "rgb(75,75,75)";
-    document.getElementById('de').style.color = "white";
+    
 }
 
 function white() {
     document.body.style.backgroundColor = "white";
-    document.getElementById('de').style.color = "black";
+   
 }
 
 function grayblue() {
     document.body.style.backgroundColor = "rgb(150,150,150)";
-    document.getElementById('de').style.color = "black";
+    
 }
 
 /* When the user clicks on the button, 
