99649c6101d7124af68a7a37df987a9002255b70
Wigge <wigge_94@live.se>
2016-06-07 10:18:14 +0200

    Nothing new, just a commit to save my work

--- WebApplication1/WebApplication1/myjs/tabs.js
+++ WebApplication1/WebApplication1/myjs/tabs.js
@@ -4,9 +4,29 @@
     // create the tab
     $('<li><a href="#tab' + nextTab + '" data-toggle="tab">Tab ' + nextTab + '</a></li>').appendTo('#tabs');
 
+
     // create the tab content
     $('<div class="tab-pane" id="tab' + nextTab + '">tab' + nextTab + ' content</div>').appendTo('.tab-content');
 
+    
+
     // make the new tab active
     $('#tabs a:last').tab('show');
+});
+
+
+$('#btnRemove').click(function (e) {
+
+    //Remove the tab
+    $('#tabs li').remove('.active');
+
+    //Make the tab after the deleted one active
+    $('#tabs a:last').tab('show');
+
+    $('#tabs li').sort('tabs li');
+
+    
+
+   
+
 });
